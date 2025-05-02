81b878e37097bf7d799b853080d9bb55c2e2c68a
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-02 12:34:25 +0330

    Build: product show page.

---
+++ public/product/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="./../assets/images/logo/logo.png" type="image/x-icon">
+	<link rel="stylesheet" href="./main.css">
+	<title>TakShop - Product</title>
+</head>
+<body>
+	<br />
+	<a href="./../index/" class="back-link">برگشت</a>
+
+	<div class="container-carts">
+		<span class="title">اسم محصول</span>
+		<br />
+		<span class="description">شرح محصول</span>
+
+		<div class="cart">
+			<div class="total-price">
+				<span>قیمت:</span>
+				<span class="badge">
+					1,000
+					تومان
+				</span>
+			</div>
+
+			<div class="total-price">
+				<span>تعداد موجود در انبار:</span>
+				<span class="badge">0</span>
+			</div>
+
+			<!-- <div class="total-price">
+				<span>تعداد سفارش شما:</span>
+				<span class="badge">0</span>
+			</div> -->
+
+			<div class="product-img">
+				<img src="./../assets/images/logo/logo.png" alt="Product Image." />
+			</div>
+
+			<div class="actions">
+				<a href="#">افزودن به سبد خرید</a>
+				<div>
+					<!-- <a href="#" class="btn-danger">حذف از سبد خرید</a> -->
+					<!-- <a href="#">+</a> -->
+					<!-- <a href="#" class="btn-danger">-</a> -->
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="./main.js"></script>
+</body>
+</html>
