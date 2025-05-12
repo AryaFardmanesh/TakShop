c3fcc251830b09699f04ddd5836ec60fa7242e23
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-12 17:45:35 +0330

    Add: database config.

---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database config
+define( 'DB_HOSTNAME', 'localhost' );
+define( 'DB_USERNAME', 'root' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_NAME', 'takshop' );
+define( 'DATABASE_INIT_FILE_ADDRESS', './../database/init.sql' );
+
+?>
