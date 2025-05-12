4c09931485c410032643d67d9a08c4f67778de05
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-12 18:52:06 +0330

    Add: account model.

---
+++ src/models/account.php
@@ -0,0 +1,29 @@
+<?php
+
+enum AccountRole: int {
+	case NORMAL	=	10;
+	case ADMIN	=	20;
+}
+
+enum AccountStatus: int {
+	case OK		=	10;
+	case BAN	=	20;
+}
+
+class AccountModel {
+	private ?string $error_message = null;
+
+	function __construct(
+		public readonly string $username,
+		public readonly string $password,
+		public readonly string $name,
+		public readonly string $phone,
+		public readonly string $zipcode,
+		public readonly string $address,
+		public readonly AccountRole $role = AccountRole::NORMAL,
+		public readonly AccountStatus $status = AccountStatus::OK,
+		public readonly string $banMessage = "",
+	) {}
+}
+
+?>
