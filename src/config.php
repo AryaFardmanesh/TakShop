44d5591f6d0f894b37114627b890309f44469feb
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-12 17:55:02 +0330

    Add: jwt sign in config file.

--- src/config.php
+++ src/config.php
@@ -7,4 +7,7 @@ define( 'DB_PASSWORD', '' );
 define( 'DB_NAME', 'takshop' );
 define( 'DATABASE_INIT_FILE_ADDRESS', './../database/init.sql' );
 
+// JWT config
+define( 'JWT_SIGN', '4813e983e6ed71c820d9e7820441c57055c06eab' );
+
 ?>
