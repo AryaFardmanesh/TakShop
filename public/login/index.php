644eefeb1375c5edbba5b7d155e79699a6a37fe0
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-14 16:45:11 +0330

    Add: service for login page.

--- public/login/index.php
+++ public/login/index.php
@@ -1,3 +1,25 @@
+<?php
+
+include_once __DIR__ . "/../../src/services/login.php";
+
+session_start();
+
+if ( $_SESSION[ "token" ] && AccountRepository::isValidToken( $_SESSION[ "token" ] ) ) {
+	header( "location:./../index/" );
+}
+
+$error_message = "";
+
+if ( LoginService::didSent() ) {
+	$result = LoginService::login();
+
+	if ( $result[ "result" ] == false ) {
+		$error_message = $result[ "message" ];
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +30,7 @@
 	<title>TakShop - Login</title>
 </head>
 <body>
-	<form action="#" method="POST" class="login-form">
+	<form action="./" method="POST" class="login-form">
 		<div class="login-form-header">
 			<span>ورود به حساب کاربری</span>
 			<img src="./../assets/images/logo/logo.png" alt="Logo image" />
@@ -16,19 +38,19 @@
 
 		<div class="login-form-input-box">
 			<span>نام کاربری</span>
-			<input type="text" placeholder="نام کاربری خود را وارد کنید" required />
+			<input type="text" name="username" placeholder="نام کاربری خود را وارد کنید" required />
 		</div>
 
 		<div class="login-form-input-box">
 			<span>رمز عبور</span>
-			<input type="password" placeholder="رمز عبور خود را وارد کنید" required />
+			<input type="password" name="password" placeholder="رمز عبور خود را وارد کنید" required />
 		</div>
 
 		<button type="submit">ورود</button>
 
 		<div class="login-form-text-center">اگر حساب کاربری ندارید یکی <a href="./../signup/">ایجاد کنید</a>.</div>
 
-		<!-- <div class="login-form-text-center error">خطلا رمز عبور اشتباه است</div> -->
+		<div class="login-form-text-center error"><?php echo $error_message; ?></div>
 	</form>
 
 	<script src="./../assets/repo/jquery-3.7.1.min.js"></script>
