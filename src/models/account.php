7cf5730728868e3682c570b966f59a39439c31ad
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-14 15:39:15 +0330

    Change: role and status type.

--- src/models/account.php
+++ src/models/account.php
@@ -2,15 +2,11 @@
 
 include_once __DIR__ . "/../utils/id.php";
 
-enum AccountRole: int {
-	case NORMAL	=	10;
-	case ADMIN	=	20;
-}
+define( 'ACCOUNT_ROLE_NORMAL', 10 );
+define( 'ACCOUNT_ROLE_ADMIN', 10 );
 
-enum AccountStatus: int {
-	case OK		=	10;
-	case BAN	=	20;
-}
+define( 'ACCOUNT_STATUS_OK', 10 );
+define( 'ACCOUNT_STATUS_BAN', 20 );
 
 class AccountModel {
 	private ?string $error_message = null;
@@ -22,8 +18,8 @@ class AccountModel {
 		public readonly string $phone,
 		public readonly string $zipcode,
 		public readonly string $address,
-		public readonly AccountRole $role = AccountRole::NORMAL,
-		public readonly AccountStatus $status = AccountStatus::OK,
+		public readonly int $role = ACCOUNT_ROLE_NORMAL,
+		public readonly int $status = ACCOUNT_STATUS_OK,
 		public readonly string $banMessage = "",
 	) {
 		$this->id = generateID();
