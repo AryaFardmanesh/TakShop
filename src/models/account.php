5c49011fca89bae91faca59373a42df7a154ee4d
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-14 16:05:30 +0330

    Remove: read only props.

--- src/models/account.php
+++ src/models/account.php
@@ -3,7 +3,7 @@
 include_once __DIR__ . "/../utils/id.php";
 
 define( 'ACCOUNT_ROLE_NORMAL', 10 );
-define( 'ACCOUNT_ROLE_ADMIN', 10 );
+define( 'ACCOUNT_ROLE_ADMIN', 20 );
 
 define( 'ACCOUNT_STATUS_OK', 10 );
 define( 'ACCOUNT_STATUS_BAN', 20 );
@@ -12,15 +12,15 @@ class AccountModel {
 	private ?string $error_message = null;
 
 	function __construct(
-		public readonly string $username,
-		public readonly string $password,
-		public readonly string $name,
-		public readonly string $phone,
-		public readonly string $zipcode,
-		public readonly string $address,
-		public readonly int $role = ACCOUNT_ROLE_NORMAL,
-		public readonly int $status = ACCOUNT_STATUS_OK,
-		public readonly string $banMessage = "",
+		public string $username,
+		public string $password,
+		public string $name,
+		public string $phone,
+		public string $zipcode,
+		public string $address,
+		public int $role = ACCOUNT_ROLE_NORMAL,
+		public int $status = ACCOUNT_STATUS_OK,
+		public string $banMessage = "",
 	) {
 		$this->id = generateID();
 	}
