cce39497dee1450f17acee1f5d1c555e3d4ed197
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-16 10:05:06 +0330

    Add: product model.

---
+++ src/models/product.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once __DIR__ . "/../utils/id.php";
+
+define( 'PRODUCT_STATUS_OK', 10 );
+define( 'PRODUCT_STATUS_BAN', 20 );
+
+class AccountModel {
+	function __construct(
+		public string $owner,
+		public string $name,
+		public string $description,
+		public int $price,
+		public int $count,
+		public string $image,
+		public int $status = PRODUCT_STATUS_OK,
+		public string $banMessage = "",
+	) {
+		$this->id = generateID();
+	}
+}
+
+?>
