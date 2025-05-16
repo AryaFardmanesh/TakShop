8cf8b302cd4ab55660a9493558cbc00812a9b889
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-16 11:43:09 +0330

    Fix: image preview in add product page.

--- scripts/addprod/main.ts
+++ scripts/addprod/main.ts
@@ -1,3 +1,23 @@
+import $ from 'jquery';
+
 document.getElementById( 'back-btn' )?.addEventListener( 'click', () => {
 	window.history.back();
+} );
+
+$( '#prodimg' ).on( 'change', event => {
+	const target = event.target as HTMLInputElement;
+	const file = target.files?.item( 0 );
+
+	if ( file ) {
+		const imageAddress = URL.createObjectURL( file );
+		const imgprev = $( '#imgprev' );
+
+		imgprev.attr( 'src', imageAddress );
+		imgprev.css( {
+			display: 'block'
+		} );
+
+		$( '#label-img-preview' ).addClass( 'label-for-file-uploaded' );
+		$( '#label-img-preview span' ).remove();
+	}
 } );
