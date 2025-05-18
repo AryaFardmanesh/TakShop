703f09289514a9de7117e6fcf5c2fde5f98e75ff
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-18 13:35:24 +0330

    Add: payment page.

--- public/payment/index.php
+++ public/payment/index.php
@@ -1,3 +1,35 @@
+<?php
+
+include_once __DIR__ . "/../../src/repositories/account.php";
+include_once __DIR__ . "/../../src/repositories/product.php";
+include_once __DIR__ . "/../../src/repositories/carts.php";
+include_once __DIR__ . "/../../src/utils/convertor.php";
+
+session_start();
+
+if ( !isset( $_SESSION[ "token" ] ) || !AccountRepository::isValidToken( $_SESSION[ "token" ] ) ) {
+	header( "location:./../login/" );
+	die;
+}
+
+$model = AccountRepository::findByToken( $_SESSION[ "token" ] );
+$carts = CartsRepository::findByUserToken( $_SESSION[ "token" ] );
+
+$totalPrice = 0;
+$totalCount = 0;
+
+if ( $carts != null ) {
+	foreach ( $carts as $cart ) {
+		$product = ProductRepository::findById( $cart[ "product_id" ] );
+		$totalPrice += ( $product->price * $cart[ "count" ] );
+		$totalCount++;
+	}
+}
+
+$totalPrice = convertPriceToReadableFormat( $totalPrice );
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,43 +50,59 @@
 			<div class="total-price">
 				<span>قیمت کل:</span>
 				<span class="badge">
-					1,000
+					<?php echo $totalPrice; ?>
 					تومان
 				</span>
 			</div>
 
 			<div class="total-price">
 				<span>تعداد محصولات:</span>
-				<span class="badge">0</span>
+				<span class="badge"><?php echo $totalCount; ?></span>
 			</div>
 
 			<div class="actions">
-				<a href="#">پرداخت</a>
+				<a href="./../../src/controllers/carts.php?action=BUY&pid=">پرداخت</a>
 			</div>
 
 			<div class="line"></div>
 
 			<div class="products">
+				<?php
+					if ( $carts != null ) {
+						foreach ( $carts as $cart ) {
+							$product = ProductRepository::findById( $cart[ "product_id" ] );
+
+							if ( $product == null ) {
+								continue;
+							}
+
+							$id = $product->id;
+							$image = $product->image;
+							$name = $product->name;
+							$price = $product->price;
+							$count = $cart[ "count" ];
+				?>
 				<div class="product-card">
-					<img src="./../assets/images/logo/logo.png" alt="Product Image." />
-					<span class="pro-name">نام محصول</span>
+					<img src="./../assets/images/products/<?php echo $image; ?>" alt="Product Image." />
+					<span class="pro-name"><?php echo $name; ?></span>
 					<div class="price">
-						<span>1,000</span>
+						<span><?php echo convertPriceToReadableFormat( $price ) ?></span>
 						<span class="badge">تومان</span>
 					</div>
 					<div class="count">
 						<span>تعداد:</span>
-						<span class="badge">1</span>
+						<span class="badge"><?php echo $count ?></span>
 					</div>
 					<div class="actions">
-						<a href="#">+</a>
-						<a href="#">-</a>
+						<a href="./../../src/controllers/carts.php?action=INC&pid=<?php echo $id; ?>">+</a>
+						<a href="./../../src/controllers/carts.php?action=DEC&pid=<?php echo $id; ?>">-</a>
 					</div>
 					<div class="btns-row">
-						<a href="#" class="btn">مشاهده</a>
-						<a href="#" class="btn-danger">حذف</a>
+						<a href="./../product/?pid=<?php echo $id; ?>" class="btn">مشاهده</a>
+						<a href="./../../src/controllers/carts.php?action=DEL&pid=<?php echo $id; ?>" class="btn-danger">حذف</a>
 					</div>
 				</div>
+				<?php } } ?>
 			</div>
 		</div>
 	</div>
